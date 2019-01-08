d762d413538398ce73acb4634a2a59efed5f5301
Samuel Ohterion <samuel.ohterion@gmx.de>
2019-01-08 20:46:19 +0100

    add conj adj for complex numbers

--- algebra.hpp
+++ algebra.hpp
@@ -18,6 +18,7 @@
 #include <initializer_list>
 #include <type_traits>
 #include <bitset>
+#include <complex>
 
 typedef std::size_t SIZE;
 
@@ -93,49 +94,49 @@ typedef Vec< long > SIDX ;
 
 
 template < typename T >
-SIZE
+inline SIZE
 n( Vec< T > const & p_vec ) {
 
 	return p_vec.size( );
 }
 
 template < typename T >
-SIZE
+inline SIZE
 nrows( Mat< T > const & p_mat ) {
 
 	return p_mat.size( );
 }
 
 template < typename T >
-SIZE
+inline SIZE
 ncols( Mat< T > const & p_mat ) {
 
 	return p_mat[ 0 ].size( );
 }
 
 template < typename T >
-void
+inline void
 assign( T & p_el, T const & p_val ) {
 
 	p_el = p_val;
 }
 
 template < typename T >
-void
+inline void
 cumsum( T & p_el, T const & p_val ) {
 
 	p_el = p_el + p_val;
 }
 
 template < typename T >
-void
+inline void
 cumprod( T & p_el, T const & p_val ) {
 
 	p_el = p_el * p_val;
 }
 
 template < typename T >
-Vec< T >
+inline Vec< T >
 & fOr( Vec< T > & p_vec, void ( *acc )( T &, T const & ), T ( *foo )( T const & ) ) {
 
 	for( auto & i : p_vec )
@@ -146,7 +147,7 @@ Vec< T >
 }
 
 template < typename T >
-Vec< T >
+inline Vec< T >
 fOr( Vec< T > & p_lhs, Vec< T > & p_rhs, void ( * acc )( T &, T const & ), T ( *foo )( T const &, T const & ) ) {
 
 	Vec< T >
@@ -169,7 +170,7 @@ fOr( Vec< T > & p_lhs, Vec< T > & p_rhs, void ( * acc )( T &, T const & ), T ( *
 }
 
 template < typename T >
-Vec< T >
+inline Vec< T >
 trnsfrm( Vec< T > const & p_vec, double ( *foo )( double const & ) ) {
 
 	Vec< T >
@@ -181,7 +182,7 @@ trnsfrm( Vec< T > const & p_vec, double ( *foo )( double const & ) ) {
 }
 
 #define BINOPVV( OP, foo ) \
-template< typename T > Vec< T > operator OP( Vec< T > const & p_lhs, Vec< T > const & p_rhs ) { Vec< T > ret( p_lhs.size( ) ); \
+template< typename T > inline Vec< T > operator OP( Vec< T > const & p_lhs, Vec< T > const & p_rhs ) { Vec< T > ret( p_lhs.size( ) ); \
 std::transform( p_lhs.cbegin( ), p_lhs.cend( ), p_rhs.cbegin( ), ret.begin( ), foo( ) ); return ret; }
 
 BINOPVV( +, std::plus< T > )
@@ -190,7 +191,7 @@ BINOPVV( *, std::multiplies< T > )
 BINOPVV( /, std::divides< T > )
 
 #define BINASSGNOPVV( OP ) \
-template< typename T > Vec< T > & operator OP( Vec< T > & p_lhs, Vec< T > const & p_rhs ) { \
+template< typename T > inline Vec< T > & operator OP( Vec< T > & p_lhs, Vec< T > const & p_rhs ) { \
 auto il = p_lhs.begin( ), el = p_lhs.end( ); auto ir = p_rhs.cbegin( ); while( il != el ) { *il OP *ir; ++il; ++ir; } return p_lhs; }
 
 BINASSGNOPVV( += )
@@ -199,7 +200,7 @@ BINASSGNOPVV( *= )
 BINASSGNOPVV( /= )
 
 #define BINOPSV( OP ) \
-template< typename T > Vec< T > operator OP( T const & p_lhs, Vec< T > p_rhs ) { for( T & v : p_rhs ) v = p_lhs OP v; return p_rhs; }
+template< typename T > inline Vec< T > operator OP( T const & p_lhs, Vec< T > p_rhs ) { for( T & v : p_rhs ) v = p_lhs OP v; return p_rhs; }
 
 BINOPSV( + )
 BINOPSV( - )
@@ -207,7 +208,7 @@ BINOPSV( * )
 BINOPSV( / )
 
 #define BINOPVS( OP ) \
-template< typename T > Vec< T > operator OP( Vec< T > p_lhs, T const & p_rhs ) { for( T & v : p_lhs ) v = v OP p_rhs; return p_lhs; }
+template< typename T > inline Vec< T > operator OP( Vec< T > p_lhs, T const & p_rhs ) { for( T & v : p_lhs ) v = v OP p_rhs; return p_lhs; }
 
 BINOPVS( + )
 BINOPVS( - )
@@ -215,7 +216,7 @@ BINOPVS( * )
 BINOPVS( / )
 
 #define BINASSGNOPVS( OP ) \
-template< typename T > Vec< T > & operator OP( Vec< T > & p_lhs, T const & p_rhs ) { for( T & v : p_lhs ) v OP p_rhs; return p_lhs; }
+template< typename T > inline Vec< T > & operator OP( Vec< T > & p_lhs, T const & p_rhs ) { for( T & v : p_lhs ) v OP p_rhs; return p_lhs; }
 
 BINASSGNOPVS( += )
 BINASSGNOPVS( -= )
@@ -223,7 +224,7 @@ BINASSGNOPVS( *= )
 BINASSGNOPVS( /= )
 
 #define BINOPMM( OP ) \
-template< typename T > Mat< T > operator OP( Mat< T > const & p_lhs, Mat< T > const & p_rhs ) { \
+template< typename T > inline Mat< T > operator OP( Mat< T > const & p_lhs, Mat< T > const & p_rhs ) { \
 Mat< T > ret( p_lhs.size( ) ); for( SIZE i = 0; i < ret.size( ); ++ i ) ret[ i ] = p_lhs[ i ] OP p_rhs[ i ];	return ret; }
 
 BINOPMM( + )
@@ -232,7 +233,7 @@ BINOPMM( * )
 BINOPMM( / )
 
 #define BINASSGNOPMM( OP ) \
-template< typename T > Mat< T > & operator OP( Mat< T > & p_lhs, Mat< T > const & p_rhs ) { \
+template< typename T > inline Mat< T > & operator OP( Mat< T > & p_lhs, Mat< T > const & p_rhs ) { \
 for( SIZE i = 0; i < p_lhs.size( ); ++ i ) p_lhs[ i ] OP p_rhs[ i ]; return p_lhs; }
 
 BINASSGNOPMM( += )
@@ -241,7 +242,7 @@ BINASSGNOPMM( *= )
 BINASSGNOPMM( /= )
 
 #define BINOPSM( OP ) \
-template< typename T > Mat< T > operator OP( T const & p_lhs, Mat< T > const & p_rhs ) { Mat< T > ret( p_rhs.size( ) ); \
+template< typename T > inline Mat< T > operator OP( T const & p_lhs, Mat< T > const & p_rhs ) { Mat< T > ret( p_rhs.size( ) ); \
 for( SIZE i = 0; i < ret.size( ); ++ i ) ret[ i ] = p_lhs OP p_rhs[ i ]; return ret; }
 
 BINOPSM( + )
@@ -250,7 +251,7 @@ BINOPSM( * )
 BINOPSM( / )
 
 #define BINOPMS( OP ) \
-template< typename T > Mat< T > operator OP( Mat< T > const & p_lhs, T const & p_rhs ) { Mat< T > ret( p_lhs.size( ) ); \
+template< typename T > inline Mat< T > operator OP( Mat< T > const & p_lhs, T const & p_rhs ) { Mat< T > ret( p_lhs.size( ) ); \
 for( SIZE i = 0; i < p_lhs.size( ); ++ i ) ret[ i ] = p_lhs[ i ] OP p_rhs; return ret; }
 
 BINOPMS( + )
@@ -259,7 +260,7 @@ BINOPMS( * )
 BINOPMS( / )
 
 #define BINASSGNOPMS( OP ) \
-template< typename T > Mat< T > & operator OP( Mat< T > & p_lhs, T const & p_rhs ) { \
+template< typename T > inline Mat< T > & operator OP( Mat< T > & p_lhs, T const & p_rhs ) { \
 for( auto & i : p_lhs ) i OP p_rhs; return p_lhs; }
 
 BINASSGNOPMS( += )
@@ -268,7 +269,7 @@ BINASSGNOPMS( *= )
 BINASSGNOPMS( /= )
 
 #define BINOPVM( OP ) \
-template< typename T > Mat< T > operator OP( Vec< T > const & p_lhs, Mat< T > const & p_rhs ) { Mat< T > ret( p_rhs.size( ) ); \
+template< typename T > inline Mat< T > operator OP( Vec< T > const & p_lhs, Mat< T > const & p_rhs ) { Mat< T > ret( p_rhs.size( ) ); \
 for( SIZE i = 0; i < ret.size( ); ++ i ) ret[ i ] = p_lhs OP p_rhs[ i ]; return ret; }
 
 BINOPVM( + )
@@ -277,7 +278,7 @@ BINOPVM( * )
 BINOPVM( / )
 
 #define BINOPMV( OP ) \
-template< typename T > Mat< T > operator OP( Mat< T > const & p_lhs, Vec< T > const & p_rhs ) { Mat< T > ret( p_lhs.size( ), Vec< T >( p_lhs[ 0 ] ) ); \
+template< typename T > inline Mat< T > operator OP( Mat< T > const & p_lhs, Vec< T > const & p_rhs ) { Mat< T > ret( p_lhs.size( ), Vec< T >( p_lhs[ 0 ] ) ); \
 for( SIZE r = 0; r < ret.size( ); ++ r ) for( SIZE c = 0; c < p_lhs[ r ].size( ); ++ c ) ret[ r ][ c ] = p_lhs[ r ][ c ] OP p_rhs[ r ]; return ret; }
 
 BINOPMV( + )
@@ -286,8 +287,9 @@ BINOPMV( * )
 BINOPMV( / )
 
 #define BINASSGNOPMV( OP ) \
-template< typename T > Mat< T > & operator OP( Mat< T > & p_lhs, Vec< T > const & p_rhs ) { \
-for( SIZE r = 0; r < nrows( p_lhs ); ++ r ) for( SIZE c = 0; c < ncols( p_lhs ); ++ c ) p_lhs[ r ][ c ] OP p_rhs[ r ]; return p_lhs; }
+template< typename T > inline Mat< T > & operator OP( Mat< T > & p_lhs, Vec< T > const & p_rhs ) { \
+SIZE rows = nrows( p_lhs ), cols = ncols( p_lhs ); \
+for( SIZE r = 0; r < rows; ++ r ) for( SIZE c = 0; c < cols; ++ c ) p_lhs[ r ][ c ] OP p_rhs[ r ]; return p_lhs; }
 
 BINASSGNOPMV( += )
 BINASSGNOPMV( -= )
@@ -295,7 +297,7 @@ BINASSGNOPMV( *= )
 BINASSGNOPMV( /= )
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 t( Mat< T > const & p_mat ) {
 
 	Mat< T >
@@ -313,28 +315,73 @@ t( Mat< T > const & p_mat ) {
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
+adj( Mat< T > p_mat ) {
+
+	SIZE
+	rows = nrows( p_mat ),
+	cols = ncols( p_mat );
+
+	for( SIZE r = 0; r < rows; ++ r ) {
+
+		for( SIZE c = 0; c < cols; ++ c ) {
+
+			p_mat[ r ][ c ] = conj( p_mat[ c ][ r ] );
+		}
+	}
+
+	return p_mat;
+}
+
+template< typename T >
+inline Mat< T >
+conj( Mat< T > p_mat ) {
+
+	SIZE
+	rows = ncols( p_mat ),
+	cols = nrows( p_mat );
+
+	for( SIZE r = 0; r < rows; ++ r ) {
+
+		for( SIZE c = 0; c < cols; ++ c ) {
+
+			p_mat[ r ][ c ] = conj( p_mat[ c ][ r ] );
+		}
+	}
+
+	return p_mat;
+}
+
+template< typename T >
+inline Mat< T >
 operator ~( Mat< T > const & p_mat ) {
 
 	return t( p_mat );
 }
 
 template< typename T >
-Vec< T >
+inline Mat< std::complex< T > >
+operator ~( Mat< std::complex< T > > const & p_mat ) {
+
+	return conj< std::complex< T > >( p_mat );
+}
+
+template< typename T >
+inline Vec< T >
 operator +( Vec< T > const & p_vec ) {
 
 	return p_vec;
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 operator +( Mat< T > const & p_mat ) {
 
 	return p_mat;
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 operator -( Vec< T > const & p_vec ) {
 
 	Vec< T >
@@ -349,7 +396,7 @@ operator -( Vec< T > const & p_vec ) {
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 operator -( Mat< T > const & p_mat ) {
 
 	Mat< T >
@@ -368,7 +415,7 @@ operator -( Mat< T > const & p_mat ) {
 
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 operator ^( Vec< T > const & p_lhs, Vec< T > const & p_rhs ) {
 
 	Mat< T >
@@ -382,49 +429,49 @@ operator ^( Vec< T > const & p_lhs, Vec< T > const & p_rhs ) {
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 operator |( T const & p_lhs, Vec< T > const & p_rhs ) {
 
 	return p_lhs * p_rhs;
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 operator |( Vec< T > const & p_lhs, T const & p_rhs ) {
 
 	return p_lhs * p_rhs;
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 & operator |=( Vec< T > & p_lhs, T const & p_rhs ) {
 
 	return p_lhs *= p_rhs;
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 operator |( T const & p_lhs, Mat< T > const & p_rhs ) {
 
 	return p_lhs * p_rhs;
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 operator |( Mat< T > const & p_lhs, T const & p_rhs ) {
 
 	return p_lhs * p_rhs;
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 & operator |=( Mat< T > & p_lhs, T const & p_rhs ) {
 
 	return p_lhs = p_lhs | p_rhs;
 }
 
 template< typename T >
-T
+inline T
 operator |( Vec< T > const & p_lhs, Vec< T > const & p_rhs ) {
 
 	T
@@ -432,24 +479,27 @@ operator |( Vec< T > const & p_lhs, Vec< T > const & p_rhs ) {
 
 	for( SIZE i = 0; i < p_lhs.size( ); ++ i  )
 
-		s = s + p_lhs[ i ] * p_rhs[ i ];
+		s +=  p_lhs[ i ] * p_rhs[ i ];
 
 	return s;
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 operator |( Vec< T > const & p_lhs, Mat< T > const & p_rhs ) {
 
-	Vec< T >
-	ret( ncols( p_rhs ) );
+	SIZE
+	cols = ncols( p_rhs );
 
-	for( SIZE c = 0; c < ncols( p_rhs ); ++ c ) {
+	Vec< T >
+	ret( cols );
+
+	for( SIZE c = 0; c < cols; ++ c ) {
 
 		T
 		sum = 0;
 
-		for( SIZE r = 0; r < nrows( p_rhs ); ++ r ) {
+		for( SIZE r = 0; r < p_lhs.size( ); ++ r ) {
 
 			sum += p_lhs[ r ] * p_rhs[ r ][ c ];
 		}
@@ -461,7 +511,7 @@ operator |( Vec< T > const & p_lhs, Mat< T > const & p_rhs ) {
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 & operator |=( Vec< T > & p_lhs, Mat< T > const & p_rhs ) {
 
 	p_lhs = p_lhs | p_rhs;
@@ -470,13 +520,16 @@ Vec< T >
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 operator |( Mat< T > const & p_lhs, Vec< T > const & p_rhs ) {
 
-	Vec< T >
-	ret( nrows( p_lhs ) );
+	SIZE
+	rows = nrows( p_lhs );
 
-	for( SIZE r = 0; r < nrows( p_lhs ); ++ r ) {
+	Vec< T >
+	ret( rows );
+
+	for( SIZE r = 0; r < rows; ++ r ) {
 
 		ret[ r ] = p_lhs[ r ] | p_rhs;
 	}
@@ -485,20 +538,26 @@ operator |( Mat< T > const & p_lhs, Vec< T > const & p_rhs ) {
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 operator |( Mat< T > const & p_lhs, Mat< T > const & p_rhs ) {
 
+	SIZE
+	rows = nrows( p_lhs ),
+	cols = ncols( p_rhs );
+
 	Mat< T >
-	ret( nrows( p_lhs ), Vec< T >( ncols( p_rhs ) ) );
+	ret( rows, Vec< T >( cols ) );
 
-	for( SIZE r = 0; r < nrows( ret ); ++ r ) {
+	T
+	s = 0;
 
-		for( SIZE c = 0; c < ncols( ret ); ++ c ) {
+	for( SIZE r = 0; r < rows; ++ r ) {
 
-			T
+		for( SIZE c = 0; c < cols; ++ c ) {
+
 			s = 0;
 
-			for( SIZE i = 0; i < nrows( p_rhs ); ++ i ) {
+			for( SIZE i = 0; i < rows; ++ i ) {
 
 				s += p_lhs[ r ][ i ] * p_rhs[ i ][ c ];
 			}
@@ -511,7 +570,7 @@ operator |( Mat< T > const & p_lhs, Mat< T > const & p_rhs ) {
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 & operator |=( Mat< T > & p_lhs, Mat< T > const & p_rhs ) {
 
 	p_lhs = p_lhs | p_rhs;
@@ -520,14 +579,14 @@ Mat< T >
 }
 
 template< typename T >
-T
-abs( Vec< T > const & p_vec ) {
+inline T
+norm( Vec< T > const & p_vec ) {
 
 	return sqrt( p_vec | p_vec );
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 eye( SIZE const & p_rows ) {
 
 	Mat< T >
@@ -543,28 +602,28 @@ eye( SIZE const & p_rows ) {
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 vcnst( SIZE const & p_rows = 1, T const & p_const = 0 ) {
 
 	return Vec< T >( p_rows, p_const );
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 mcnst( SIZE const & p_rows = 1, SIZE const & p_cols = 0, T const & p_const = 0 ) {
 
 	return Mat< T >( p_rows, Vec< T >( p_cols == 0 ? p_rows : p_cols, p_const ) );
 }
 
 template< typename T >
-Tsr< T >
+inline Tsr< T >
 tcnst( SIZE const & p_cells = 1, SIZE const & p_rows = 0, SIZE const & p_cols = 0, T const & p_const = 0 ) {
 
 	return Tsr< T >( p_cells, Mat< T >( p_rows == 0 ? p_cells : p_rows, p_cols, p_const ) );
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 vrnd( SIZE const & p_size ) {
 
 	Vec< T >
@@ -578,7 +637,7 @@ vrnd( SIZE const & p_size ) {
 }
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 mrnd( SIZE const & p_rows, SIZE const & p_cols = 0 ) {
 
 	Mat< T >
@@ -592,7 +651,7 @@ mrnd( SIZE const & p_rows, SIZE const & p_cols = 0 ) {
 }
 
 template< typename T >
-Tsr< T >
+inline Tsr< T >
 trnd( SIZE const & p_cells, SIZE const & p_rows = 0, SIZE const & p_cols = 0 ) {
 
 	Tsr< T >
@@ -606,7 +665,7 @@ trnd( SIZE const & p_cells, SIZE const & p_rows = 0, SIZE const & p_cols = 0 ) {
 }
 
 template< typename T >
-SIZE
+inline SIZE
 maxStringLengthOfElements( Vec< T > const & p_vec ) {
 
 	SIZE
@@ -633,7 +692,7 @@ maxStringLengthOfElements( Vec< T > const & p_vec ) {
 }
 
 template< typename T >
-std::string
+inline std::string
 vec2Str( Vec< T > const & p_vec, int const & p_width = 0 ) {
 
 	std::stringstream
@@ -652,7 +711,7 @@ vec2Str( Vec< T > const & p_vec, int const & p_width = 0 ) {
 }
 
 template< typename T >
-std::ostream
+inline std::ostream
 & operator << ( std::ostream & p_os, Vec< T > const & p_vec ) {
 
 	for( auto v : p_vec )
@@ -663,7 +722,7 @@ std::ostream
 }
 
 template< typename T >
-std::ostream
+inline std::ostream
 & operator << ( std::ostream & p_os, Mat< T > const & p_mat ) {
 
 	SIZE
@@ -692,7 +751,7 @@ std::ostream
 }
 
 template< typename T >
-std::ostream
+inline std::ostream
 & operator << ( std::ostream & p_os, Vec< Mat< T > > const & p_ten ) {
 
 	for( auto v : p_ten )
@@ -703,7 +762,7 @@ std::ostream
 }
 
 // print a string
-void
+inline void
 print( std::string const & p_string ) {
 
 	std::cout << p_string << std::endl;
@@ -711,7 +770,7 @@ print( std::string const & p_string ) {
 
 // print a string and a vector or a matrix or a tensor
 template< typename T >
-void
+inline void
 print( std::string const & p_string, T const & p_v ) {
 
 	print( p_string );
@@ -719,7 +778,7 @@ print( std::string const & p_string, T const & p_v ) {
 	std::cout << p_v << std::endl << std::endl;
 }
 
-SIZE
+inline SIZE
 idxOfNxt( SIZE const & p_val, SIZE p_idx, Vec< SIZE > const & p_vec ) {
 
 	while( p_idx < p_vec.size( ) ) {
@@ -735,13 +794,13 @@ idxOfNxt( SIZE const & p_val, SIZE p_idx, Vec< SIZE > const & p_vec ) {
 	return p_idx;
 }
 
-SIZE
+inline SIZE
 idxOf1st( SIZE const & p_val, Vec< SIZE > const & p_vec ) {
 
 	return idxOfNxt( p_val, 0, p_vec );
 }
 
-Vec< SIZE >
+inline Vec< SIZE >
 permutationZero( SIZE const & p_size ) {
 
 	Vec< SIZE >
@@ -755,7 +814,7 @@ permutationZero( SIZE const & p_size ) {
 	return permutation;
 }
 
-Vec< SIZE >
+inline Vec< SIZE >
 & nextPermutation( Vec< SIZE > & p_permutation ) {
 
 	SIZE
@@ -795,13 +854,13 @@ Vec< SIZE >
 	return p_permutation;
 }
 
-SIZE
+inline SIZE
 fac( SIZE p_n ) {
 
 	return p_n == 0 ? 1 : p_n * fac( p_n - 1 );
 }
 
-SIZE
+inline SIZE
 findID( unsigned long long const & p_idx, SIZE p_from = 0 ) {
 
 	while ( p_from < ( sizeof ( unsigned long long ) << 3 ) ) {
@@ -817,7 +876,7 @@ findID( unsigned long long const & p_idx, SIZE p_from = 0 ) {
 }
 
 template< typename T >
-T
+inline T
 detRec( Mat< T > const & p_m, SIZE const & p_row, unsigned long long & p_cidx ) {
 
 	SIZE
@@ -860,7 +919,7 @@ detRec( Mat< T > const & p_m, SIZE const & p_row, unsigned long long & p_cidx ) 
 }
 
 template< typename T >
-T
+inline T
 detRec( Mat< T > const & p_m ) {
 
 	SIZE
@@ -908,7 +967,7 @@ class Slice {
 */
 
 template< typename T >
-T
+inline T
 detPerm( Mat< T > const & p_m, Vec< SIZE > & p_ridx, Vec< SIZE > & p_cidx ) {
 
 	if( nrows( p_m ) != ncols( p_m ) )
@@ -946,7 +1005,7 @@ detPerm( Mat< T > const & p_m, Vec< SIZE > & p_ridx, Vec< SIZE > & p_cidx ) {
 }
 
 template< typename T >
-T
+inline T
 detPerm( Mat< T > const & p_m ) {
 
 	Vec< SIZE >
@@ -987,7 +1046,7 @@ class LRData {
 };
 
 template< typename T >
-LRData< T >
+inline LRData< T >
 decompLR( Mat< T > const & p_m ) {
 
 	LRData< T >
@@ -1038,7 +1097,7 @@ decompLR( Mat< T > const & p_m ) {
 
 			for( SIZE row = col + 1; row < size; ++ row ) {
 
-				double
+				T
 				f = factor * lrdata.mat[ row ][ col ];
 
 				lrdata.mat[ row ][ col ] = f;
@@ -1055,7 +1114,7 @@ decompLR( Mat< T > const & p_m ) {
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 solve( LRData< T > const & p_lr, Vec< T > const & p_v ) {
 
 	Vec< T >
@@ -1097,8 +1156,24 @@ solve( LRData< T > const & p_lr, Vec< T > const & p_v ) {
 	return x;
 }
 
+
 template< typename T >
-T
+inline Mat< T >
+solve( LRData< T > const & p_lr, Mat< T > const & p_m ) {
+
+	Mat< T >
+	m = mcnst< T >( p_lr.size, p_lr.size );
+
+	for( SIZE r = 0; r < p_lr.size; ++r ) {
+
+		m[ r ] = solve( p_lr, p_m[ r ] );
+	}
+
+	return m;
+}
+
+template< typename T >
+inline T
 det( Mat< T > const & p_m ) {
 
 	LRData< T >
@@ -1160,25 +1235,13 @@ inv2( Mat< T > const & p_m ) {
 */
 
 template< typename T >
-Mat< T >
+inline Mat< T >
 inv( Mat< T > const & p_m ) {
 
-	LRData< T >
-	l_r = decompLR( p_m );
-
-	Mat< T >
-	e = eye< T >( l_r.size ),
-	d = e;
-
-	for( SIZE i = 0; i < l_r.size; ++ i ) {
-
-		d[ i ] = solve( l_r, e[ i ] );
-	}
-
-	return ~d;
+	return t( solve( decompLR( p_m ), eye< T >( n( p_m ) ) ) );
 }
 
-double
+inline double
 round( double const & p_v, int const & p_digits = 0 ) {
 
 	double
@@ -1192,7 +1255,7 @@ round( double const & p_v, int const & p_digits = 0 ) {
 }
 
 template< typename T >
-Vec< T >
+inline Vec< T >
 round( Vec< T > const & p_v, int const & p_digits = 0 ) {
 
 	Vec< T >
@@ -1207,7 +1270,7 @@ round( Vec< T > const & p_v, int const & p_digits = 0 ) {
 }
 
 template < typename T >
-Vec< T >
+inline Vec< T >
 sub ( Vec< T > const & p_vec, int const & p_begin, int const & p_size ) {
 
 	Vec< T >
@@ -1222,7 +1285,7 @@ sub ( Vec< T > const & p_vec, int const & p_begin, int const & p_size ) {
 }
 
 template < typename T >
-Vec< T >
+inline Vec< T >
 sub ( Vec< T > const & p_vec, IDX const & p_idx ) {
 
 	Vec< T >
@@ -1237,7 +1300,7 @@ sub ( Vec< T > const & p_vec, IDX const & p_idx ) {
 }
 
 template < typename T >
-Mat< T >
+inline Mat< T >
 sub ( Mat< T > const & p_mat, int const & p_row, int const & p_col, int const & p_rows, int const & p_cols ) {
 
 	Mat< T >
@@ -1252,7 +1315,7 @@ sub ( Mat< T > const & p_mat, int const & p_row, int const & p_col, int const & 
 }
 
 template < typename T >
-Mat< T >
+inline Mat< T >
 sub ( Mat< T > const & p_mat, IDX const & p_ridx, IDX const & p_cidx ) {
 
 	Mat< T >
